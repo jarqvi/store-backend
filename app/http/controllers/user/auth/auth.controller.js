08e0e04a5b016eddeca3e26836f9705246aee8de
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-05-14 18:02:19 +0330

    Add OTP code

--- app/http/controllers/user/auth/auth.controller.js
+++ app/http/controllers/user/auth/auth.controller.js
@@ -1,14 +1,55 @@
+const { UserModel } = require("../../../../models/users");
+const { EXPIRES_IN, USER_ROLE } = require("../../../../utils/constans");
+const { randomNumberGenerator } = require("../../../../utils/functions");
 const { authSchema } = require("../../../validators/user/auth.schema")
 const createError = require('http-errors');
+const Controller = require("../../controller");
 
-class UserAuthController {
+class UserAuthController extends Controller {
     async login(req, res, next) {
         try {
-            const result = await authSchema.validateAsync(req.body);
-            return res.status(200).send('Your login was successful.');
+            await authSchema.validateAsync(req.body);
+            const { mobile } = req.body;
+            const code = randomNumberGenerator();
+            const result = await this.saveUser(mobile, code);
+            if (!result) throw createError.Unauthorized('Your login was failed.');
+            return res.status(200).send({
+                data: {
+                    statusCode: 200,
+                    message: 'OTP sent successfully.',
+                    code,
+                    mobile
+                }
+            });
         } catch (error) {
             next(createError.BadRequest(error.message));
         }
+    }
+    async saveUser(mobile, code) {
+        let otp = {
+            code,
+            expiresIn: EXPIRES_IN
+        }
+        const result = await this.checkExitsUser(mobile);
+        if (result) {
+            return await (this.updateUser(mobile, {otp}))
+        }
+        return !!(await UserModel.create({
+            mobile,
+            otp,
+            Roles: [USER_ROLE]
+        })) 
+    }
+    async checkExitsUser(mobile) {
+        const user = await UserModel.findOne({ mobile });
+        return !!user;
+    }
+    async updateUser(mobile, objectData = {}) {
+        Object.keys(objectData).forEach(key => {
+            if (['', ' ', 0, null, undefined, '0', NaN].includes(objectData[key])) delete objectData[key];
+        });
+        const updateResult = await UserModel.updateOne({mobile}, {$set: objectData});
+        return !!updateResult.modifiedCount;
     }
 }
 
--- app/router/user/auth.js
+++ app/router/user/auth.js
@@ -1,6 +1,35 @@
 const { UserAuthController } = require('../../http/controllers/user/auth/auth.controller');
-
 const router = require('express').Router();
+
+/**
+ * @swagger
+ * tags:
+ *  name: User-Auth
+ *  description: Login user section
+ */
+/**
+ * @swagger
+ * /user/login:
+ *      post:
+ *          summary: User Login
+ *          tags: [User-Auth]
+ *          description: Login user in user panel with phone number and one time password(OTP) 
+ *          parameters:
+ *          -   name: mobile
+ *              description: fa-IRI mobile number
+ *              in: formData
+ *              required: true
+ *              type: string
+ *          responses:
+ *              201:
+ *                  description: User login successfully
+ *              400:
+ *                  description: Bad request
+ *              401:
+ *                  description: Unauthorized
+ *              500:
+ *                  description: Internal server error
+ */
 
 router.post('/login', UserAuthController.login)
 
