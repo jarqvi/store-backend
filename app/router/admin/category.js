e6c18a2a3922867169121d2316807ffe6c7f158b
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-05-27 19:18:42 +0330

    Add delete & get routes

--- app/router/admin/category.js
+++ app/router/admin/category.js
@@ -21,6 +21,60 @@ const router = require('express').Router();
  *                  description: Success
  */
 router.post('/add', CategoryController.addCategory);
+/**
+ * @swagger
+ *  /admin/category/parents:
+ *      get:
+ *          tags: [Admin-Panel]
+ *          summery: Get all parent categories
+ *          responses:
+ *              200:
+ *                  description: Success
+ */
+router.get('/parents', CategoryController.getAllParents);
+/**
+ * @swagger
+ *  /admin/category/children/{parent}:
+ *      get:
+ *          tags: [Admin-Panel]
+ *          summery: Get all child categories
+ *          parameters:
+ *              -   in: path
+ *                  name: parent
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: Success
+ */
+router.get('/children/:parent', CategoryController.getChildOfParents);
+/**
+ * @swagger
+ *  /admin/category/all:
+ *      get:
+ *          tags: [Admin-Panel]
+ *          summery: Get all categories
+ *          responses:
+ *              200:
+ *                  description: Success
+ */
+router.get('/all', CategoryController.getAllCategory);
+/**
+ * @swagger
+ *  /admin/category/delete/{id}:
+ *      delete:
+ *          tags: [Admin-Panel]
+ *          summery: Delete category by id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              202:
+ *                  description: Success
+ */
+router.delete('/delete/:id', CategoryController.removeCategory);
 
 module.exports = {
     categoryRoutes: router
