c0820125bf1877285c9285b42c3083285e43cd45
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-06-29 18:24:44 +0330

    Add example response for gitListCourses route

--- app/router/admin/swagger/course.swagger.js
+++ app/router/admin/swagger/course.swagger.js
@@ -49,8 +49,45 @@
  *                      $ref: '#/components/schemas/Types'
  *                      
  */
-
-
+/**
+ * @swagger
+ *  definitions:
+ *      ListOfCourses:
+ *          type: array
+ *          items:
+ *              type: object
+ *              properties:
+ *                  _id:
+ *                      type: string
+ *                      example: "649c022a50430f4c56d94867"
+ *                  title:
+ *                      type: string
+ *                      example: "title of course"
+ *                  shortText:
+ *                      type: string
+ *                      example: "shortText of course"
+ *                  text:
+ *                      type: string
+ *                      example: "text of course"
+ *                  status:
+ *                      type: string
+ *                      example: "notStarted | Completed | Holding"
+ *                  time:
+ *                      type: string
+ *                      example: "01:20:35"
+ *                  price:
+ *                      type: integer
+ *                      example: 250000
+ *                  discount:
+ *                      type: integer
+ *                      example: 20
+ *                  studentCount:
+ *                      type: integer
+ *                      example: 500
+ *                  teacher:
+ *                      type: string
+ *                      example: "John Doe"
+ */
 /**
  * @swagger
  *  /admin/courses/list:
@@ -65,6 +102,10 @@
  *          responses:
  *              200:
  *                  description: successfully
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/ListOfCourses'
  */
 /**
  * @swagger
